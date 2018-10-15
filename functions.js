48bd3c7b329bf693556ecd117a5aa5a05cc44921
Jorrin Bruns <Jorrin.Bruns@gmail.com>
2018-10-15 14:56:53 -0400

    first push

---
+++ functions.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+    $('.main-carousel').flickity({
+        cellAlign: 'left',
+        wrapAround: true,
+        prevNextButtons: false
+    });
+
+});
+
+function buttonClick(){
+    window.alert("Thank you for Subscribing");
+}
+
+function addToCart(){
+    
+}
