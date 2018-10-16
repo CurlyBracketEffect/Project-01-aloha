f7a177e43aac7750997e4e28b167370b9559d4b5
Jorrin Bruns <Jorrin.Bruns@gmail.com>
2018-10-16 10:46:57 -0400

    This is the final version....I think

--- functions.js
+++ functions.js
@@ -13,5 +13,35 @@ function buttonClick(){
 }
 
 function addToCart(){
-    
+    var cartItems = document.getElementById('cartItems');
+    cartItems.setAttribute("style", "display:block");
+    var number = parseInt(cartItems.innerHTML);
+    ++number;
+    cartItems.innerHTML = number;
+}
+var marginY= 0;
+var destination = 0;
+var speed = 5;
+var scroller = null;
+var windowScrollTop =0;
+
+function initScroll(elementId){
+    destination = document.getElementById(elementId).offsetTop;
+    destination = destination - 102;
+
+    scroller = setTimeout(function(){
+        initScroll(elementId);
+    }, 1);
+
+    marginY = marginY + speed;
+
+    if(marginY>=destination){
+        clearTimeout(scroller);
+        marginY = 0;
+    }
+    if(marginY!=0){
+    window.scroll(0, marginY);}
+{
+
+}
 }
